e5c7d7b56e2146656c4091f339a769e803767cd3
linxcreative <162355084+linxcreative@users.noreply.github.com>
2024-05-18 22:32:33 +0600

    Add files via upload

---
+++ string.php
@@ -0,0 +1,16 @@
+<?php
+
+$strings = ["Hello", "World", "PHP", "Programming"];
+
+foreach ($strings as $string) {
+    // Count vowels
+    $vowelCount = preg_match_all('/[aeiou]/i', $string, $matches);
+
+    // Reverse string
+    $reversedString = strrev($string);
+
+    // Print results
+    echo "Original String: $string, Vowel Count: $vowelCount, Reversed String: $reversedString\n";
+}
+
+?>
